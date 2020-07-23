d1e5720bb404c4e0df690d1c8fbf1b4fa1c58ed1
morteza <mortezaghorbani3@gmail.com>
2020-07-23 21:10:14 +0430

    first commit

---
+++ app/src/main/java/com/morteza/projectmusicplayer/MainActivity.java
@@ -0,0 +1,14 @@
+package com.morteza.projectmusicplayer;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
